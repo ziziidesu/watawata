58a912f42346ba0457e20e6f6fedabf2bf64c266
Glitch (wakame02q) <none>
2024-08-30 04:49:52 +0000

    🌆👕 Checkpoint ./index.js:58362660/1645

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function getYouTubePageTitle(url) {
 
 // /titleエンドポイントにアクセスしたときにページタイトルを表示
 app.get('/title', async (req, res) => {
-  const videoUrl = 'https://www.youtube.com/watch?v=f6TytcA47rI'; // 対象のYouTube動画のURL
+  const videoUrl = 'https://www.youtube.com/watch?v=f6TytcA47rI';
   const pageinfo = await getYouTubePageTitle(videoUrl);
   res.setHeader('Content-Type', 'text/plain');
   res.send(`${pageinfo}`);
@@ -65,38 +65,11 @@ app.get('/holo', async (req, res) => {
 });
 
 //わかめわかめ
-app.get('/wakame', (req, res) => {
-  res.send(`
-    <form action="/wakame/go" method="post">
-      <label for="url">URLを入力してください:</label>
-      <input type="text" id="url" name="url" required>
-      <button type="submit">確定</button>
-    </form>
-  `);
+app.get('/mimi', async (req, res) => {
+  const videoUrl = 'https://www.youtube.com/watch?v=-2KzcaiyBFo&vl=ja';
+  const pageinfo = await getYouTubePageTitle(videoUrl);
+  res.send(`${pageinfo}`);
 });
-
-app.post('/wakame/go', async (req, res) => {
-  const videoUrl = req.body.url;
-  const pageinfo = await getPageinfo(videoUrl);
-  res.send(`ページタイトル: ${pageinfo}`);
-});
-
-async function getPageinfo(url) {
-  try {
-    // YouTubeページのHTMLを取得
-    const { data } = await axios.get(url);
-    const pageinfo = data;
-
-    // <title>タグの内容を正規表現で抽出
-    const titleMatch = data.match(/<title>(.*?)<\/title>/);
-    const title = titleMatch ? titleMatch[1] : 'タイトルが取得できませんでした';
-    
-    return pageinfo;
-  } catch (error) {
-    console.error('Error fetching YouTube data:', error);
-    return 'エラーが発生しました';
-  }
-}
 
 //サジェスト
 app.use(cors());
