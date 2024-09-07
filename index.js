9cc5a65b524c154ae64582b3819822f1a4e88c58
Glitch (wakame02q) <none>
2024-09-07 00:40:27 +0000

    👄🎆 Checkpoint ./index.js:58362660/95 ./views/infowatch.ejs:58362660/204

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ app.get('/w/:id', async (req, res) => {
     const viewsMatch = html.match(/"views":\{.*?"simpleText":"(.*?)"/);
     const channelImageMatch = html.match(/"channelThumbnail":\{.*?"url":"(.*?)"/);
     const channelNameMatch = html.match(/"channel":\{.*?"simpleText":"(.*?)"/);
-    const channnelIdMatch = html.match(/"browseEndpoint":\{.*?"browseId":["(.*?)"]/);
+    const channnelIdMatch = html.match(/"subscribeEndpoint":\{.*?"channelIds":"(.*?)"/);
 
     const videoTitle = titleMatch ? titleMatch[1] : 'タイトルを取得できませんでした';
     const videoDes = descriptionMatch ? descriptionMatch[1].replace(/\\n/g, '\n') : '概要を取得できませんでした';
