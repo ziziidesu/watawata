2383bcacb40288a2e8b65629a512042bc070670f
Glitch (wakame02m) <none>
2024-09-20 01:08:28 +0000

    ☄️⛴ Checkpoint ./index.js:58362660/51 ./views/search.ejs:58362660/2 ./views/search2.ejs:58362660/4 ./views/blog/end.ejs:58362660/932 ./views/blog/list.ejs:58362660/200

--- index.js
+++ index.js
@@ -261,12 +261,12 @@ app.get('/api/login/:id', async (req, res) => {
 // Invidiousインスタンスのリスト
 const invidiousInstances = [
   "https://inv.nadeko.net","https://invidious.ethibox.fr","https://iv.datura.network",
-  "https://invidious.jing.rocks","https://invidious.reallyaweso.me","https://invidious.protokolla.fi","https://invidious.perennialte.ch",
+  "https://invidious.jing.rocks","https://invidious.reallyaweso.me","https://inv.phene.dev","https://invidious.protokolla.fi","https://invidious.perennialte.ch",
   "https://invidious.materialio.us","https://yewtu.be","https://invidious.fdn.fr",
   "https://inv.tux.pizza","https://invidious.privacyredirect.com","https://invidious.drgns.space",
   "https://vid.puffyan.us","https://vid.puffyan.us","https://inv.riverside.rocks",
   "https://invidio.xamh.de","https://y.com.sb","https://invidious.sethforprivacy.com",
-  "https://invidious.tiekoetter.com","https://inv.bp.projectsegfau.lt/","https://inv.vern.cc",
+  "https://invidious.tiekoetter.com","https://inv.bp.projectsegfau.lt","https://inv.vern.cc",
   "https://invidious.nerdvpn.de","https://invidious.private.coffee"
 ];
 
