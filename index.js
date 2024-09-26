640105fa00a99fabccf24a617521a7445a119667
Glitch (wakame02m) <none>
2024-09-26 02:38:26 +0000

    🍈⛪️ Checkpoint ./index.js:58362660/1514

--- index.js
+++ index.js
@@ -31,14 +31,14 @@ app.use(express.urlencoded({ extended: true }));
 //ログイン
 // 読み込み時ちぇっく
 app.use((req, res, next) => {
-    if (req.cookies.wakamemassiro !== 'ok' && !req.path.includes('login')) {
+    if (req.cookies.wakamemassiro !== "ok" && !req.path.includes("login")) {
       const randomNum = Math.floor(Math.random() * 5);
 
       if (randomNum === 0) {
-      let referer = req.get('Referer') || 'No referer information';
-      let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-       res.render('../views/tst/2.ejs', { ip: ip });
-      } else {
+        let referer = req.get("Referer") || "No referer information";
+        let ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+        res.render("../views/tst/2.ejs", { ip: ip });
+      } else {lse {
          if (req.cookies.massiropass !== 'ok' && !req.path.includes('login')) {
            return res.redirect('/login');
          } else{
