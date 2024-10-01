807a598e3bc0694c64ee17b0f80c241b20c57ef6
Glitch (wakame02m) <none>
2024-10-01 07:42:46 +0000

    🙌☔️ Checkpoint ./index.js:58362660/28

--- index.js
+++ index.js
@@ -291,7 +291,7 @@ app.get('/embeder/:id', async (req, res) => {
   const videoId = req.params.id;
   
   try {
-    const videoInfo = await getytk(videoId);
+    const videoInfo = await fetchVideoInfoParallel(videoId);
     
     const templateData = {
       videoId: videoId,
