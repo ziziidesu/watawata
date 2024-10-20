c0632469c15cf8b28c66c00ccb435e8b7c82bd1e
Glitch (peppermint-peppered-jay) <none>
2024-10-20 09:01:56 +0000

    🍘🌴 Checkpoint ./index.js:58362660/4

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const supabaseUrl = 'process.env.SUPABASE_URL';
-const supabaseKey = 'process.env.SUPABASE_KEY'; 
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY; 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 
