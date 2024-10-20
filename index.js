42b046648223c07f44ecd43c4e91e0eead7e0eee
Glitch (peppermint-peppered-jay) <none>
2024-10-20 05:02:19 +0000

    🏁👽 Checkpoint ./index.js:58362660/3490 ./package.json:58362660/28 ./views/tst/1.ejs:58362660/10 ./views/index.html:58362660/772

--- index.js
+++ index.js
@@ -997,7 +997,42 @@ app.get('/wakamehistory', (req, res) => {
 });
 
 //サジェスト
+const http = require('http');
+app.get('/suggest', (req, res) => {
+    const keyword = req.query.keyword;
+    const options = {
+        hostname: 'www.google.com',
+        path: `/complete/search?client=youtube&hl=ja&ds=yt&q=${encodeURIComponent(keyword)}`,
+        method: 'GET',
+        headers: {
+            'User-Agent': 'Mozilla/5.0'
+        }
+    };
+    const request = http.request(options, (response) => {
+        let data = '';
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+        response.on('end', () => {
+            const jsonString = data.substring(data.indexOf('['), data.lastIndexOf(']') + 1);
 
+            try {
+                const suggestionsArray = JSON.parse(jsonString);
+                const suggestions = suggestionsArray[1].map(i => i[0]);
+                res.setHeader('Content-Type', 'application/json; charset=utf-8');
+                res.json(suggestions);
+            } catch (error) {
+                console.error('JSON parse error:', error);
+                res.status(500).send({ error: 'えらー。あらら' });
+            }
+        });
+    });
+    request.on('error', (error) => {
+        console.error('Request error:', error);
+        res.status(500).send({ error: 'えらー。あらら' });
+    });
+    request.end();
+});
 
 
 //概要欄用リダイレクト
